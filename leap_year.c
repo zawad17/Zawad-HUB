143cbce83cda17bd978eb928b65718c78f20466c
Zawad Rahman <138771190+zawad17@users.noreply.github.com>
2024-01-30 00:41:56 +0600

    Create leap_year.c

---
+++ leap_year.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int year ;
+
+    printf("Enter a year : ");
+    scanf("%d",&year);
+
+    if (year%400 ==0 )
+        printf("Leap Year");
+
+    else if (year%4 ==0 && year%100 !=0)
+        printf("leap year ");
+
+    else
+        printf("Not Leap Year ");
+
+    return 0;
+
+}
