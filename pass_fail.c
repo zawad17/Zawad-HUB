60c426a9ac4e214eb9fe81c3589550e11da30427
Zawad Rahman <138771190+zawad17@users.noreply.github.com>
2024-01-30 00:47:33 +0600

    Create pass_fail.c

---
+++ pass_fail.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int mark;
+    
+    printf("Enter your mark : ");
+    scanf("%d",&mark);
+    
+    if( mark >=33)
+        printf("Pass");
+        
+    else
+        printf("Fail");
+}
